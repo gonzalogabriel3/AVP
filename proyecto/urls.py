1afe657a03c285e87d3e236e4cfbf80ca0c9eef9
gonzalogabriel3 <ggcruzado@udc.edu.ar>
2019-01-07 08:48:41 -0300

    agrego vista de inicio del proyecto

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,5 +9,8 @@ urlpatterns = [
     path(r'^select2/', include('django_select2.urls')),
     #Agrego autenticacion de usuarios provista por Django a la app "pasajes"
     path('pasajes/', include('django.contrib.auth.urls')),
+
+    #Url a index del proyecto
+    path('inicio/', TemplateView.as_view(template_name='templates/indexProyecto.html'),name='inicio'),
 
 ]
