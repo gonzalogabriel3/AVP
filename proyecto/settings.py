dd7d9667da80acb6e84412e8261114903923258f
gonzalogabriel3 <ggcruzado@udc.edu.ar>
2019-02-01 11:53:36 -0300

    Agrego templates,css,js en la carpeta 'appPersonal'

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets'),
+    os.path.join(BASE_DIR, 'proyectoApp/templates'),
 ]
 
 LOGIN_REDIRECT_URL = '/pasajes'
