9061fdf2b1f9e0854357aae00249124f20000362
gonzalogabriel3 <ggcruzado@udc.edu.ar>
2019-05-14 08:38:32 -0300

    Agrego abm de sanciones

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -95,10 +95,10 @@ DATABASES = {
         'NAME': 'personal',
         'USER': 'postgres',
         'PASSWORD': 'sistemasavp',
-        #'HOST': '172.155.0.8',
-        #'PORT': '5432',
-        'HOST': 'sysavp.chubut.gov.ar',
-        'PORT': '33060',
+        'HOST': '172.155.0.8',
+        'PORT': '5432',
+        #'HOST': 'sysavp.chubut.gov.ar',
+        #'PORT': '33060',
         
     }
 }
